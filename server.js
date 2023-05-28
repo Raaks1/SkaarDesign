41cb26cb782fc129e50a8110eed5148187f80c5a
Joar Fonn Skaar <88654732+Raaks1@users.noreply.github.com>
2023-05-28 13:26:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.listen(5000, () => console.log("Server Running"));
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "jfonnskaar@gmail.com",
-    pass: "",
+    user: "****@gmail.com",
+    pass: "********",
   },
 });
 
