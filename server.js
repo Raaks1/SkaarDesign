5ba80353ef620c9ab994e7a36f4a09d76e772552
Raaks1 <88654732+Raaks1@users.noreply.github.com>
2023-05-30 13:47:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.listen(5000, () => console.log("Server Running"));
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "******@gmail.com",
-    pass: "*****",
+    user: "jfonnskaar@gmail.com",
+    pass: "ozwuoidtcuhjlmfc",
   },
 });
 
